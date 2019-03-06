bd7f3404c6c6fd5c263ff4baba26596a7495eed7
Katba-Caroline <katba.caroline@gmail.com>
2019-03-06 23:23:51 +0000

    Modify run.py

--- app/run.py
+++ app/run.py
@@ -1,13 +1,15 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+import plotly.graph_objs as go
+from plotly.graph_objs import Bar, Heatmap, Scatter
 from sklearn.externals import joblib
 import pickle
 from sqlalchemy import create_engine
@@ -44,6 +46,26 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    ###########################
+    # Number of Messages per Category
+    Message_counts = df.drop(['id','message','original','genre', 'related'], axis=1).sum().sort_values()
+    Message_names = list(Message_counts.index)
+    
+    ###########################
+    
+    # Top ten categories count
+    top_category_count = df.iloc[:,4:].sum().sort_values(ascending=False)[1:11]
+    top_category_names = list(top_category_count.index)
+    
+    ###########################
+    
+    # extract categories
+    category_map = df.iloc[:,4:].corr().values
+    category_names = list(df.iloc[:,4:].columns)
+    
+    
+    #############################
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -64,7 +86,66 @@ def index():
                     'title': "Genre"
                 }
             }
-        }
+        },
+        
+        #############################
+        {
+            'data': [
+                Bar(
+                    x=Message_counts,
+                    y=Message_names,
+                    orientation = 'h',
+                    
+                
+                )
+            ],
+           
+            'layout': {
+                'title': 'Number of Messages per Category',
+                
+                'xaxis': {
+                    'title': "Number of Messages"
+                    
+                },
+            }
+        },
+      ########################
+        {
+            'data': [
+                Bar(
+                    x=top_category_names,
+                    y=top_category_count
+                )
+            ],
+
+            'layout': {
+                'title': 'Top Ten Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        },
+         #############################
+        {
+            'data': [
+                Heatmap(
+                    x=category_names,
+                    y=category_names[::-1],
+                    z=category_map
+                )    
+            ],
+
+            'layout': {
+                'title': 'Correlation Heatmap of Categories',
+                'xaxis': {'tickangle': -45}
+            }
+        },
+        #############################
+       
+      ########################
     ]
     
     # encode plotly graphs in JSON
